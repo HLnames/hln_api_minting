b2be1aa0dabbfe06f76c040c7876660ecc86cc80
yhoni <153692313+siftor-yhoni@users.noreply.github.com>
2025-09-16 22:49:46 -0700

    fix: add slippage calc to Forge script & correct value var name.
    Signed-off-by: yhoni <153692313+siftor-yhoni@users.noreply.github.com>

--- example/mintWithApiArgs.s.sol
+++ example/mintWithApiArgs.s.sol
@@ -9,6 +9,9 @@ interface IHLN_RefMinter {
 }
 
 contract MintScript is Script {
+    uint256 constant SLIPPAGE_PERCENTAGE = 2; // 2%
+    uint256 constant HUNDRED = 100;
+
     function run() public {
     // Requires env var TESTNET_MINTER_ADDRESS=0x6284e439514604B9eaAbc8537330d3E4f0313Df6
     IHLN_RefMinter minterContract = IHLN_RefMinter(vm.envAddress("TESTNET_MINTER_ADDRESS"));
@@ -18,14 +21,16 @@ contract MintScript is Script {
     uint256 durationInYears = 1;
     bytes memory sig = hex"6cd690efdd4bac5620a0eb882714c74ac4a691942409db30e1b6c4f2d547737d6da20dc2cfb58ae8b803c0a916fc130cf74edd455d228b50e2fcef9b4709d4061c";
     uint256 timestamp = 1734380244;
-    uint256 tieredPrice = 1000000000000000000; // 1 HYPE in wei
+    uint256 amountRequired = 1000000000000000000; // 1 HYPE in wei
     string memory token = "native";
     bytes32 referral_hash = 0x16cf12aee280fca6f9552bf861dbd966fb9b5ddfceee7d568888e461e0178016;
 
-    // Calculate native payment in HYPE from tierFee
-    uint256 requiredPayment = (tieredPrice * 1e18) / minterContract.getPriceOfNativeInUSD();
+    // Add in 2% slippage to the HYPE/USD rate, in case of oracle volatility.
+    // N.B. Should be refunded to payer if unfilled
+    uint256 paymentValue = (amountRequired * (HUNDRED + SLIPPAGE_PERCENTAGE)) / HUNDRED;
+
     // Mint with HYPE + referral
-    minterContract.mintWithNative{value: requiredPayment}(label, durationInYears, sig, timestamp, referral_hash);
+    minterContract.mintWithNative{value: paymentValue}(label, durationInYears, sig, timestamp, referral_hash);
 
     // Pass bytes32(0) in referral_hash for "no-referral" mode
     }
